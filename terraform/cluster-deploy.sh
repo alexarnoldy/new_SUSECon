46ce8fd0e01bd1e8217e0711c9c3e1bce64d316d
aarnoldy <alexarnoldy@gmail.com>
2020-01-31 11:09:39 -0800

    working on cluster-destroy.sh script

---
+++ terraform/cluster-deploy.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+## Script to deploy CaaSP clusters via Terraform across a set group of KVM hosts
+
+###
+Variables
+###
+TF_DIR=/home/admin/new_SUSECon/terraform
+STATE_DIR=/home/admin/new_SUSECon/terraform/state
+QEMU_USER=admin
+QEMU_HOST_PREFIX=infra
+DOMAIN=susecon.local
+ACTION=apply
+###
+
+DEPLOYorDESTROY="$(basename $0)"
+[ $DEPLOYorDESTROY = cluster-destroy.sh ] && ACTION="destroy"
+
+echo ""
+echo "***IMPORTANT*** This script does not know the number of hosts you have"
+echo "in your environment. Operating on the wrong hosts, too many hosts, "
+echo "or hosts that don't exist can result in anything"
+echo "from errors, to headaches, to an urgent desire to update to one's resume"
+echo ""
+echo "Enter the host numbers for deployment in formats of single number (i.e. 1),"
+read -p "space separated list (i.e. 1 3), or range (i.e. 2..4): " HOSTS
+
+case $HOSTS in
+	*..*)
+		eval '
+		for EACH in {'"$HOSTS"'}; do
+			cd ${TF_DIR}; terraform ${ACTION} -state=${STATE_DIR}/${QEMU_HOST_PREFIX}${EACH}.tfstate -var libvirt_uri="qemu+ssh://${QEMU_USER}@${QEMU_HOST_PREFIX}${EACH}.${DOMAIN}/system"
+		done
+		'
+		;;
+	*)
+		for EACH in $(echo ${HOSTS})
+		do
+			cd ${TF_DIR}; terraform ${ACTION} -state=${STATE_DIR}/${QEMU_HOST_PREFIX}${EACH}.tfstate -var libvirt_uri="qemu+ssh://${QEMU_USER}@${QEMU_HOST_PREFIX}${EACH}.${DOMAIN}/system"
+		done
+		;;
+esac
